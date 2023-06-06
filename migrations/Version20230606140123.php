4a23d92a71387aafb6b0b4843d6537b03f782f4e
David Chosson <davidcho868@gmail.com>
2023-06-06 16:05:42 +0200

    25 creation des migrations

---
+++ migrations/Version20230606140123.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230606140123 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE `admin` (id INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE adopter (id INT NOT NULL, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(50) NOT NULL, city VARCHAR(50) NOT NULL, department VARCHAR(50) NOT NULL, phone VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE anoucement (id INT AUTO_INCREMENT NOT NULL, breeder_id INT NOT NULL, title VARCHAR(255) NOT NULL, info LONGTEXT NOT NULL, date_anoucement DATETIME NOT NULL, INDEX IDX_A61404533C95BB1 (breeder_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE breeder (id INT NOT NULL, name VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE dog (id INT AUTO_INCREMENT NOT NULL, anoucement_id INT NOT NULL, name VARCHAR(50) NOT NULL, background VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, is_tolerant TINYINT(1) NOT NULL, is_lof TINYINT(1) NOT NULL, INDEX IDX_812C397DDADC94DA (anoucement_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE dog_request (dog_id INT NOT NULL, request_id INT NOT NULL, INDEX IDX_B999564D634DFEB (dog_id), INDEX IDX_B999564D427EB8A5 (request_id), PRIMARY KEY(dog_id, request_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE message (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, request_id INT NOT NULL, date_message DATETIME NOT NULL, text LONGTEXT NOT NULL, INDEX IDX_B6BD307FA76ED395 (user_id), INDEX IDX_B6BD307F427EB8A5 (request_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE picture (id INT AUTO_INCREMENT NOT NULL, dog_id INT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, link VARCHAR(255) NOT NULL, INDEX IDX_16DB4F89634DFEB (dog_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE race (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE race_dog (race_id INT NOT NULL, dog_id INT NOT NULL, INDEX IDX_942774876E59D40D (race_id), INDEX IDX_94277487634DFEB (dog_id), PRIMARY KEY(race_id, dog_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE request (id INT AUTO_INCREMENT NOT NULL, annoucement_id INT NOT NULL, adopter_id INT NOT NULL, INDEX IDX_3B978F9F32B110F5 (annoucement_id), INDEX IDX_3B978F9FA0D47673 (adopter_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE `admin` ADD CONSTRAINT FK_880E0D76BF396750 FOREIGN KEY (id) REFERENCES user (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE adopter ADD CONSTRAINT FK_A6ECDA1DBF396750 FOREIGN KEY (id) REFERENCES user (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE anoucement ADD CONSTRAINT FK_A61404533C95BB1 FOREIGN KEY (breeder_id) REFERENCES breeder (id)');
+        $this->addSql('ALTER TABLE breeder ADD CONSTRAINT FK_73DA3D7ABF396750 FOREIGN KEY (id) REFERENCES user (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE dog ADD CONSTRAINT FK_812C397DDADC94DA FOREIGN KEY (anoucement_id) REFERENCES anoucement (id)');
+        $this->addSql('ALTER TABLE dog_request ADD CONSTRAINT FK_B999564D634DFEB FOREIGN KEY (dog_id) REFERENCES dog (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE dog_request ADD CONSTRAINT FK_B999564D427EB8A5 FOREIGN KEY (request_id) REFERENCES request (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307FA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307F427EB8A5 FOREIGN KEY (request_id) REFERENCES request (id)');
+        $this->addSql('ALTER TABLE picture ADD CONSTRAINT FK_16DB4F89634DFEB FOREIGN KEY (dog_id) REFERENCES dog (id)');
+        $this->addSql('ALTER TABLE race_dog ADD CONSTRAINT FK_942774876E59D40D FOREIGN KEY (race_id) REFERENCES race (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE race_dog ADD CONSTRAINT FK_94277487634DFEB FOREIGN KEY (dog_id) REFERENCES dog (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE request ADD CONSTRAINT FK_3B978F9F32B110F5 FOREIGN KEY (annoucement_id) REFERENCES anoucement (id)');
+        $this->addSql('ALTER TABLE request ADD CONSTRAINT FK_3B978F9FA0D47673 FOREIGN KEY (adopter_id) REFERENCES adopter (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE `admin` DROP FOREIGN KEY FK_880E0D76BF396750');
+        $this->addSql('ALTER TABLE adopter DROP FOREIGN KEY FK_A6ECDA1DBF396750');
+        $this->addSql('ALTER TABLE anoucement DROP FOREIGN KEY FK_A61404533C95BB1');
+        $this->addSql('ALTER TABLE breeder DROP FOREIGN KEY FK_73DA3D7ABF396750');
+        $this->addSql('ALTER TABLE dog DROP FOREIGN KEY FK_812C397DDADC94DA');
+        $this->addSql('ALTER TABLE dog_request DROP FOREIGN KEY FK_B999564D634DFEB');
+        $this->addSql('ALTER TABLE dog_request DROP FOREIGN KEY FK_B999564D427EB8A5');
+        $this->addSql('ALTER TABLE message DROP FOREIGN KEY FK_B6BD307FA76ED395');
+        $this->addSql('ALTER TABLE message DROP FOREIGN KEY FK_B6BD307F427EB8A5');
+        $this->addSql('ALTER TABLE picture DROP FOREIGN KEY FK_16DB4F89634DFEB');
+        $this->addSql('ALTER TABLE race_dog DROP FOREIGN KEY FK_942774876E59D40D');
+        $this->addSql('ALTER TABLE race_dog DROP FOREIGN KEY FK_94277487634DFEB');
+        $this->addSql('ALTER TABLE request DROP FOREIGN KEY FK_3B978F9F32B110F5');
+        $this->addSql('ALTER TABLE request DROP FOREIGN KEY FK_3B978F9FA0D47673');
+        $this->addSql('DROP TABLE `admin`');
+        $this->addSql('DROP TABLE adopter');
+        $this->addSql('DROP TABLE anoucement');
+        $this->addSql('DROP TABLE breeder');
+        $this->addSql('DROP TABLE dog');
+        $this->addSql('DROP TABLE dog_request');
+        $this->addSql('DROP TABLE message');
+        $this->addSql('DROP TABLE picture');
+        $this->addSql('DROP TABLE race');
+        $this->addSql('DROP TABLE race_dog');
+        $this->addSql('DROP TABLE request');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
