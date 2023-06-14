a15c910084d5e3779b7e4515e397007dc542173e
David Chosson <davidcho868@gmail.com>
2023-06-14 15:29:00 +0200

    16 crea formulaire mon compte adoptant debut pour modifier les données des adopter

---
+++ src/Form/AdopterType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Adopter;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdopterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', 
+            EmailType::class,
+            [
+                'required' => true,
+            ]
+            )
+            ->add('firstName',
+            TextType::class,
+            [
+                'required' => true,
+            ])
+            ->add('lastName',
+            TextType::class,
+            [
+                'required' => true,
+            ])
+            ->add('city',
+            TextType::class,
+            [
+                'required' => true,
+            ])
+            ->add('department',
+            TextType::class,
+            [
+                'required' => true,
+            ])
+            ->add('phone')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Adopter::class,
+        ]);
+    }
+}
