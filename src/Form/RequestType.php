56f99ec011fe3eff9ef4e72bc4c089bb14499d6e
HueKhuc <khuchue.hus@gmail.com>
2023-06-15 15:14:02 +0200

    Ticket 13 : Creation Godog #13

---
+++ src/Form/RequestType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Dog;
+use App\Entity\Request;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RequestType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('dogs', EntityType::class, [
+                'class' => Dog::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'required'   => true,
+            ])
+            ->add('messages', CollectionType::class, [
+                'entry_type' => MessageType::class,
+                'required'   => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Request::class,
+        ]);
+    }
+}
