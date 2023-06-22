587b2cc02ba223f41145a9c5f3570a7845f650a8
komu <komupeter16@yahoo.com>
2023-06-22 14:22:41 +0200

    44 modifie motdepasse breeder

--- src/Controller/BreederController.php
+++ src/Controller/BreederController.php
@@ -6,10 +6,13 @@ use App\Entity\Message;
 use App\Form\BreederType;
 use App\Repository\AnnouncementRepository;
 use App\Repository\MessageRepository;
+use App\Repository\RequestRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\ModifPasswordType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
@@ -19,9 +22,11 @@ class BreederController extends AbstractController
     #[IsGranted('ROLE_BREEDER')]
     public function modifyBreed(
         Request $request,
+        RequestRepository $requestRepository,
         EntityManagerInterface $entityManager,
         AnnouncementRepository $announcementRepository,
         MessageRepository $messageRepository,
+        UserPasswordHasherInterface $userPasswordHasher
     ): Response {
         /** @var \App\Entity\Breeder $breeder  */
         $breeder = $this->getUser();
@@ -44,9 +49,31 @@ class BreederController extends AbstractController
             $this->addFlash('success', 'data inserted');
             return $this->redirectToRoute("app_breeder");
         }
+        // modify breeder password
+        $formPassword = $this->createForm(ModifPasswordType::class, $breeder);
+        $formPassword->handleRequest($request);
+        if ($formPassword->isSubmitted() && $formPassword->isValid()) {
+            $breederPassword = $breeder->getPlainPassword();
+
+            $breeder->setPassword(
+
+                $userPasswordHasher->hashPassword(
+                    $breeder,
+                    $breederPassword
+                )
+            );
+
+            $entityManager->persist($breeder);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Password Changed');
+            return $this->redirectToRoute('app_breeder');
+
+        }
         // dd($breederAnnouncements);
         return $this->render('breeder/index.html.twig', [
             'form' => $form->createView(),
+            'formPassword' => $formPassword->createView(),
             'breederAnnouncements' => $breederAnnouncements,
             'messages' => $messages,
             // 'breederMessages' => $breederMessages,
--- src/Form/ModifPasswordType.php
+++ src/Form/ModifPasswordType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Adopter;
+use App\Entity\Breeder;
+use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
@@ -14,23 +16,22 @@ class ModifPasswordType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            
+
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'invalid_message' => 'The password fields must match.',
                 'options' => ['attr' => ['class' => 'password-field']],
                 'required' => true,
-                'first_options'  => ['label' => 'Password'],
+                'first_options' => ['label' => 'Password'],
                 'second_options' => ['label' => 'Repeat Password'],
             ])
-
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Adopter::class,
+            'data_class' => User::class,
         ]);
     }
 }
